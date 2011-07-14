6888fdd1960fccad915f1e73d3d94db302de1d58
Stephen Touset <stephen@touset.org>
2011-07-14 12:39:42 -0400

    Begin phasing out DATA usage

---
+++ test/euler/problem_008_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+describe 'Problem 008' do
+  it 'should find the greatest product of five consecutive digits in a ,1000
+      digit number' do
+    problem_008.must_equal 40824
+  end
+end
