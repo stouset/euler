0325f2eae5a5466c38763cdc526dd93b97c020d6
Stephen Touset <stephen@touset.org>
2011-07-14 11:13:05 -0400

    Test for Problem 4

---
+++ test/euler/problem_004_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+describe 'Problem 004' do
+  it 'should find the largest palindrome of the product of two 3-digit numbers.' do
+    problem_004.must_equal '906609'
+  end
+end
