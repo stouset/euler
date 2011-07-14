bd6720d6e6f057fb9376a6b76455a25dd14776c4
Stephen Touset <stephen@touset.org>
2011-07-14 15:42:28 -0400

    Test for Problem 11

---
+++ test/euler/problem_011_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+describe 'Problem 011' do
+  it 'should find the greatest product of four adjacent numbers in a 20x20 grid' do
+    problem_011.must_equal 70600674
+  end
+end
