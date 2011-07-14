53eea25fceb737247ed78110ce3ca692509e9155
Stephen Touset <stephen@touset.org>
2011-07-14 12:07:24 -0400

    Test for Problem 6

---
+++ test/euler/problem_006_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+describe 'Problem 006' do
+  it 'should compute the difference between the sum of the squares of 1 to 100
+      and the square of the sum' do
+    problem_006.must_equal 25164150
+  end
+end
