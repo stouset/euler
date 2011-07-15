8343fe6af169bdc8d2e04b71b71a647fcc325c1a
Stephen Touset <stephen@touset.org>
2011-07-15 11:16:34 -0400

    Test for Problem 14

---
+++ test/euler/problem_014_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+describe 'Problem 014' do
+  it 'should find the number under 1,000,000 with the largest Collatz
+      chain' do
+    problem_014.must_equal 837799
+  end
+end
