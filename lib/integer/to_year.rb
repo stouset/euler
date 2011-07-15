59bd385d8f33b8c9084f57f688078c6994e605d7
Stephen Touset <stephen@touset.org>
2011-07-15 12:09:17 -0400

    Problem 19

---
+++ lib/integer/to_year.rb
@@ -0,0 +1,5 @@
+class Integer
+  def to_year
+    Date.new(self)
+  end
+end
---
+++ test/euler/problem_019_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+describe 'Problem 019' do
+  it 'should find the number of Sundays that fell on the first of the month
+      during the twentieth century' do
+    problem_019.must_equal 171
+  end
+end
