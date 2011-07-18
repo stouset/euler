b0ceaa9775ffd54e916786ac308a74ca908bb015
Stephen Touset <stephen@touset.org>
2011-07-18 16:52:07 -0400

    Style fixes

--- test/euler/problem_004_test.rb
+++ test/euler/problem_004_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 describe 'Problem 004' do
-  it 'should find the largest palindrome of the product of two 3-digit numbers.' do
+  it 'should find the largest palindrome of the product of two 3-digit 
+      numbers' do
     problem_004.must_equal 906609
   end
 end
--- test/euler/problem_009_test.rb
+++ test/euler/problem_009_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 describe 'Problem 009' do
-  it 'should find the product of the only Pythagorean triplet whose sum is 1,000' do
+  it 'should find the product of the only Pythagorean triplet whose sum is
+      1,000' do
     problem_009.must_equal 31875000
   end
 end
