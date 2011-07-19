747af7f223a6da5d911d6696221a9b852521dd38
Stephen Touset <stephen@touset.org>
2011-07-19 14:04:20 -0400

    Comments

--- lib/integer/aliquot_sum.rb
+++ lib/integer/aliquot_sum.rb
@@ -3,6 +3,9 @@ class Integer
   # http://bit.ly/qWBaiN
   #
   def aliquot_sum
+    # equivalent to `self.sigma - self`, but this is noticeably faster until
+    # we find a definition of sigma that can use a variant of this method's
+    # inner loop
     self.factorize.multiply do |prime, exponent|
       (prime ** (exponent + 1) - 1) / (prime - 1)
     end - self
